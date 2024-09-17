276e2a02c48c8f31084c7321d2482c1f24cc1395
Daniel Oliveira <oliveiradb1@gmail.com>
2024-09-17 00:09:00 -0300

    Infra: add frontend infra

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+     origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
-  )
+  );
+
   await app.listen(3000);
 }
 bootstrap();
